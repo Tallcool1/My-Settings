8dc4e91bbfff05c3f4fba991727006ddbae4c509
Tracy Fisher <webstudent@PC16278.local>
2015-03-26 18:05:28 -0500

    Initial Commit

---
+++ My Settings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Settings
+//
+//  Created by webstudent on 3/26/15.
+//  Copyright (c) 2015 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
